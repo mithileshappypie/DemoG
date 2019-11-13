15fbf9452b8f6b12d83c9b4ea3f0cda312f44733
Mithilesh <mithilesh@appypiellp.com>
2019-11-13 19:50:55 +0530

    setup

---
+++ DemoG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoG
+//
+//  Created by Mithilesh on 13/11/19.
+//  Copyright © 2019 Mithilesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
